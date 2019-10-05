5d35ce4eb44188e49d6ce8136f53cf4d9a0a1c79
Mostafa Haque <93haque@gmail.com>
2019-10-05 11:28:07 -0400

    Upgraded unity for real

---
+++ LudumDare2019Ghosted/Assets/Scripts/DialogueClass.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueClass
+{
+    public string Dialogue;
+    public AudioClip audioClip;
+    public Sprite sprite;
+}
---
+++ LudumDare2019Ghosted/Assets/Scripts/DialogueScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Dialogue Class")]
+public class DialogueScriptableObject : ScriptableObject
+{
+    public List<DialogueClass> dialogueList;
+    
+}
